0951d2e0813a9611b7deff2d93e39f17ec0bca09
ikechi Greatness <ikechigreat@gmail.com>
2022-11-06 15:37:08 +0100

    edited config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const DATABASE_CONNECT_STRING = process.env.DATABASE_CONNECT_STRING
-const PORT = process.env.PORT
-const SECRET = process.env.SECRET
+require("dotenv").config();
+const DATABASE_CONNECT_STRING = process.env.DATABASE_CONNECT_STRING;
+const PORT = 8080 || process.env.PORT;
+const SECRET = process.env.SECRET;
 
 module.exports = {
   DATABASE_CONNECT_STRING,
   PORT,
   SECRET,
-}
+};
